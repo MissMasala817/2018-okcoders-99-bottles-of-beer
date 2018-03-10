65b9bfaa81734dbb069ce586ad4112c296858032
Masala Thomas <34103496+MissMasala817@users.noreply.github.com>
2018-03-10 12:39:47 -0600

    Update 99-bottle-remix.js

--- 99-bottle-remix.js
+++ 99-bottle-remix.js
@@ -24,3 +24,19 @@ for (var x=99;x>0;x--) {
 // 	//console.log(beers + " No more bottles of beer on the wall!  Now, lets do it with shots!!\n");
 // 	beers = printVerse(beers);
 }                      
+
+var printVerse = function (num) {
+
+	var name = "bottles";
+	if (num === 1) { name = "bottles"; }
+
+	console.log(num + " " + name + " of beer on the wall");
+	console.log(num + " " + name + " bottles of beer");
+	console.log("you take one down and pass it around");
+	num--;
+	console.log(num + " bottles of beer on the wall\n");
+}
+
+for (var x = 99; x > 0; x--) {
+	printVerse(x);
+}
